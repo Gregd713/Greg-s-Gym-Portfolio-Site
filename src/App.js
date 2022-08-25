62f96475ddff0f08e74994169732a49d226ea746
Gregory Delgado <gregd92694@yahoo.com>
2022-08-25 16:12:02 -0500

    Floor and Bulb added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import { Canvas } from 'react-three-fiber';
-import Floor from './components/Floor';
+import {Physics} from '@react-three/cannon';
+import Bulb from './components/Bulb'
+import Plane from './components/Floor';
 import Orbit from './components/Orbit';
 import './App.css';
 
@@ -8,9 +10,13 @@ function App() {
   return (
   <div style={{height:'100vh',width:'100vw'}}>
     <Canvas style={{background:'black'}} camera={{position:[7,7,7]}}>
+      <Physics>
       <Orbit/>
       <axesHelper args={[5]}/>
-      
+      <Plane position = {[0,-0.5,0]} />
+      </Physics>
+      <Bulb position={[0,3,0]}/>
+
     </Canvas>
   </div>
   );
